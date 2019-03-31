d2192e55e8314665bbf9dfa9a6dda8bd10d20ee9
huhn <sehe89@gmail.com>
2019-03-31 22:37:38 +0200

    add demo buttons

--- src/Components/Sections/SectionEcosystem.js
+++ src/Components/Sections/SectionEcosystem.js
@@ -10,40 +10,43 @@ import customer from '../../assets/customer.svg'
 export default props =>
   <div className="section section__light" id="ecosystem">
     <div className="container">
-      <Layout.Row type="flex" justify="center" style={{marginBottom: 50}}>
-        <Layout.Col  span="12" className="center">
+      <Layout.Row type="flex" justify="center" style={{ marginBottom: 50 }}>
+        <Layout.Col span="12" className="center">
           <Fade top>
             <h2>Ecosystems</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
           </Fade>
         </Layout.Col>
       </Layout.Row>
       <Layout.Row gutter="60" >
-        <Layout.Col  span="8" className="center">
+        <Layout.Col span="8" className="center">
           <Flip left>
             <div className="card">
-              <img alt="Vendor" src={vendor}/>
+              <img alt="Vendor" src={vendor} />
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <a class="el-button" href="https://vendor-demo.nile.community" target="_blank">Demo</a>
+            </div>
+          </Flip>
+        </Layout.Col>
+        <Layout.Col span="8" className="center">
+          <Flip left>
+            <div className="card">
+              <img alt="supplier" src={supplier} />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
           </Flip>
         </Layout.Col>
-        <Layout.Col  span="8" className="center">
+        <Layout.Col span="8" className="center">
           <Flip left>
             <div className="card">
-              <img alt="supplier" src={supplier}/>
+              <img alt="Customer" src={customer} />
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            </div>
-          </Flip>
-        </Layout.Col>
-        <Layout.Col  span="8" className="center">
-          <Flip left>
-            <div className="card">
-              <img alt="Customer" src={customer}/>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+              <a class="el-button" href="https://customer-demo.nile.community" target="_blank">Demo</a>
             </div>
           </Flip>
         </Layout.Col>
