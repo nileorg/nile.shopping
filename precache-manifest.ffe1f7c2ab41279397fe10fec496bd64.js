2542d51f172584b875f95bc3ec5f54794bc79b5e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-11 14:37:26 +0000

    Deploy nileorg/nile.shopping to github.com/nileorg/nile.shopping.git:gh-pages

---
+++ precache-manifest.ffe1f7c2ab41279397fe10fec496bd64.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d4923bda6a4394f5a9f",
+    "url": "/static/css/main.b7f4d197.chunk.css"
+  },
+  {
+    "revision": "7d4923bda6a4394f5a9f",
+    "url": "/static/js/main.9f324d0e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ca45ec324af1f783471e",
+    "url": "/static/css/2.760ad471.chunk.css"
+  },
+  {
+    "revision": "ca45ec324af1f783471e",
+    "url": "/static/js/2.4ba1e99e.chunk.js"
+  },
+  {
+    "revision": "28063f52ef3ff60c23cdd6860f4b4f24",
+    "url": "/static/media/nile_logo.28063f52.svg"
+  },
+  {
+    "revision": "145aef094d0e355b22c72f24b91b2ba0",
+    "url": "/static/media/nile_typo.145aef09.svg"
+  },
+  {
+    "revision": "1f4198aa17769fb2c735124a7e591c8a",
+    "url": "/static/media/heart.1f4198aa.svg"
+  },
+  {
+    "revision": "afce63c40e1ed50a77263c6e827e6cde",
+    "url": "/static/media/home_hero.afce63c4.jpg"
+  },
+  {
+    "revision": "47e18eaa9609d3b91c090527e232a117",
+    "url": "/static/media/earth.47e18eaa.svg"
+  },
+  {
+    "revision": "2e88927b98b9a9ee9a62ae739bdb7e67",
+    "url": "/static/media/divider_grey.2e88927b.svg"
+  },
+  {
+    "revision": "524c73627e58ed63b0e4291b0b5ce1b6",
+    "url": "/static/media/caret-down.524c7362.svg"
+  },
+  {
+    "revision": "00506569b8ba96e5849c1c72d7f7f965",
+    "url": "/static/media/discord.00506569.svg"
+  },
+  {
+    "revision": "45a854952e391a054ece629d8169a04d",
+    "url": "/static/media/divider.45a85495.svg"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "df55f5f24ec3c252141b5d4468d17839",
+    "url": "/index.html"
+  }
+];
