63c716e8620b413e256e03a0bdfd3494d421e59e
huhn <sehe89@gmail.com>
2019-04-11 15:26:55 +0200

    add buyer text

--- src/Components/Pages/Buyer.js
+++ src/Components/Pages/Buyer.js
@@ -30,7 +30,7 @@ const heroSmallData = {
 const videoPromoData = {
   id: 'buyer-promo',
   title: 'Buyer',
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  description: 'More and more people get aware of the importance of local economies. Empowering people with a platform to have a great service quality but supporting local people instead of giant corporations has the potential to transform the awareness into action.'
 }
 const shopData = {
   id: 'shop',
@@ -40,26 +40,26 @@ const shopData = {
   image_path: '../assets/mockups/buyer/shop_cart.jpg',
   image_right: true,
   title: 'Shop',
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  description: "This is the shop page in Nile! Here you see all the products, you can filter them by category or name. You can find also information about the shop and the best offers!",
   button: false,
   link_to: '',
   button_text: '',
   items: [
     {
-      title: 'Highlight I',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Products',
+      description: 'First you get an overview of the discounts and best offers, then you get a list of all the products, you can filter it by name or category!',
     },
     {
-      title: 'Highlight II',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Shop information',
+      description: 'You see all the shop information: location, description, opening time, social links and more. Just read more!',
     },
     {
-      title: 'Highlight III',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Pages',
+      description: 'A seller can create useful pages to give you more information about the products or the store.',
     },
     {
-      title: 'Highlight IV',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Shop App',
+      description: 'If you like the shop and you always buys products from it, you can install the shop application in your device! Next time you don’t have to search to it into Nile!',
     }
   ]
 }
@@ -71,26 +71,18 @@ const productData = {
   image_path: '../assets/mockups/buyer/product.jpg',
   image_right: false,
   title: 'Product',
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  description: 'Learn more about the product reading the description, the availability and other information.',
   button: false,
   link_to: '',
   button_text: '',
   items: [
     {
-      title: 'Highlight I',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Product details',
+      description: 'You find all information which you need to know about the product.',
     },
     {
-      title: 'Highlight II',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      title: 'Highlight III',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      title: 'Highlight IV',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Certifications',
+      description: 'Every product can have certifications such as local ingredients, handicraft, bio: so you better know what you buy.',
     }
   ]
 }
@@ -132,15 +124,15 @@ const paymentData = {
   image_type: 'mobile',
   image_path: '../assets/mockups/buyer/payment.jpg',
   image_right: false,
-  title: 'Payment',
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  title: 'Flexible Payment System',
+  description: 'You can choose to pay with the payment systems accepted by the store.',
   button: false,
   link_to: '',
   button_text: '',
   items: [
     {
-      title: 'Highlight I',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Tags',
+      description: 'Each payment system is described by tags, green if good, red if bad. For example “feeless” that stands for a feeless payment system, or “fee 0.3%” that stands for a payment system that has fees).',
     },
     {
       title: 'Highlight II',
@@ -163,27 +155,19 @@ const deliveryData = {
   image_type: 'mobile',
   image_path: '../assets/mockups/buyer/confirm_delivery.jpg',
   image_right: true,
-  title: 'Delivery',
-  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  title: 'Boosted 🚀 Order Status',
+  description: 'Nile offers powerful components that allows you to receive detailed information about your order. This information are updated in real time by the shop and the delivery person.',
   button: false,
   link_to: '',
   button_text: '',
   items: [
     {
-      title: 'Highlight I',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Feedback',
+      description: 'Whenever your package moves, you can see it on the map. There are a lot of ways in which the seller can show information to you. You will receive a notification each time there is an update!',
     },
     {
-      title: 'Highlight II',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      title: 'Highlight III',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    },
-    {
-      title: 'Highlight IV',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      title: 'Confirm delivery',
+      description: 'When the delivery person arrives, he will ask you to verify that the order was successful. You just scan the qr-code on his phone and the order gets completed. Also the seller receive a notification about the successful transaction.',
     }
   ]
 }
